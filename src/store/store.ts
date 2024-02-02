a1102f9ace885f2fc11477f84af2814ef25883a8
theDone <muzodone5@gmail.com>
2024-02-02 03:32:30 +0300

    Added increasing and decreasing the amount of products in the cart and added order history

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,13 @@
-import {create} from 'zustand';
-import {produce} from 'immer';
-import {persist, createJSONStorage} from 'zustand/middleware';
+import { create } from 'zustand';
+import { produce } from 'immer';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CoffeeData from '../data/CoffeeData';
 import BeansData from '../data/BeansData';
 
 export const useStore = create(
   persist(
-    (set,get ) => ({
+    (set, get) => ({
       CoffeeList: CoffeeData,
       BeanList: BeansData,
       CartPrice: 0,
@@ -61,7 +61,7 @@ export const useStore = create(
                 tempprice =
                   tempprice +
                   parseFloat(state.CartList[i].prices[j].price) *
-                    state.CartList[i].prices[j].quantity;
+                  state.CartList[i].prices[j].quantity;
               }
               state.CartList[i].ItemPrice = tempprice.toFixed(2).toString();
               totalprice = totalprice + tempprice;
@@ -134,6 +134,74 @@ export const useStore = create(
             }
             state.FavoritesList.splice(spliceIndex, 1);
           }),
+        ),
+      incrementCartItemQuantity: (id: string, size: string) =>
+        set(
+          produce(state => {
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id == id) {
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (state.CartList[i].prices[j].size == size) {
+                    state.CartList[i].prices[j].quantity++;
+                    break;
+                  }
+                }
+              }
+            }
+          })
+        ),
+      decrementCartItemQuantity: (id: string, size: string) =>
+        set(
+          produce(state => {
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id == id) {
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (state.CartList[i].prices[j].size == size) {
+                    if (state.CartList[i].prices.length > 1) {
+                      if (state.CartList[i].prices[j].quantity > 1) {
+                        state.CartList[i].prices[j].quantity--;
+                      } else {
+                        state.CartList[i].prices.splice(j, 1);
+                      }
+                    } else {
+                      if (state.CartList[i].prices[j].quantity > 1) {
+                        state.CartList[i].prices[j].quantity--;
+                      } else {
+                        state.CartLit.splice(i, 1);
+                      }
+                    }
+                    break;
+                  }
+                }
+              }
+            }
+          })
+        ),
+      addToOrderHistoryListFromCart: () =>
+        set(produce(state => {
+          let temp = state.CartList.reduce(
+            (accumulator: number, currentValue: any) =>
+              accumulator + parseFloat(currentValue.ItemPrice),
+            0,
+          )
+          let currentCartListTotalPrice = temp.toFixed(2).toString();
+          if (state.OrderHistoryList.length > 0) {
+            state.OrderHistoryList.unshift({
+              OrderDate: new Date().toDateString() + " " + new Date().toLocaleTimeString(),
+              CartList: state.CartList,
+              CartListPrice: temp.toFixed(2).toString(),
+
+            })
+          } else {
+            state.OrderHistoryList.push({
+              OrderDate: new Date().toDateString() + " " + new Date().toLocaleTimeString(),
+              CartList: state.CartList,
+              CartListPrice: temp.toFixed(2).toString(),
+
+            })
+          }
+          state.CartList = [];
+        })
         ),
     }),
     {
